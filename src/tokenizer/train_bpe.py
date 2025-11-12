d1b1080a19993082ceafe8c7c2f3d41f34c8a544
Hadi Sheikhi <hadsheikhi@gmail.com>
2025-11-12 22:06:40 +0000

    create a script to train bpe

---
+++ src/tokenizer/train_bpe.py
@@ -0,0 +1,18 @@
+import argparse
+from src.tokenizer.bpe.bpe import BPETokenizer
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input_file", required=True)
+    parser.add_argument("--output_dir", required=True)
+    parser.add_argument("--vocab_size", type=int, default=512)
+    parser.add_argument("--special_tokens", nargs="+", default=["<|endoftext|>"])
+
+    args = parser.parse_args()
+
+    tokenizer = BPETokenizer(args.input_file, args.vocab_size, args.special_tokens)
+
+    tokenizer.train_bpe()
+
+    tokenizer.save_state(args.output_dir)
