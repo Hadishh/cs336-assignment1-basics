fix softmax bug

src/nn/transformer.py
@@ -1,6 +1,6 @@
 import torch
 from src.nn.tranfromer_layer import TransformerLayer
-from src.nn.utils import RotaryPositionalEmbeddings, Embedding, RMSNorm, Linear, softmax
+from src.nn.utils import RotaryPositionalEmbeddings, Embedding, RMSNorm, Linear
 
 
 class Transformer(torch.nn.Module):
@@ -17,8 +17,6 @@ class Transformer(torch.nn.Module):
         dtype=None,
     ):
         super().__init__()
-
-        self.num_layers = num_layers
 
         self.rope = RotaryPositionalEmbeddings(
             d_k=d_model // num_heads,
@@ -55,4 +53,4 @@ class Transformer(torch.nn.Module):
 
         x = self.lm_head(x)
 
-        return softmax(x, dim=-1)
+        return x

src/nn/utils.py
@@ -132,7 +132,7 @@ class RotaryPositionalEmbeddings(torch.nn.Module):
         cos = einops.einsum(x, cos, "... seq_len d, ... seq_len d -> ... seq_len d")
         sin = einops.einsum(temp, sin, "... seq_len d, ... seq_len d -> ... seq_len d")
 
-        return sin + cos
+        return (sin + cos).to(device=x.device, dtype=x.dtype)
 
 
 def softmax(in_features: torch.Tensor, dim: int):
@@ -212,8 +212,9 @@ class CausalMultiHeadSelfAttention(torch.nn.Module):
             Q = self.RoPE(Q, token_positions)
             K = self.RoPE(K, token_positions)
 
-        mask = torch.ones((seq_len, seq_len), device=Q.device)
+        mask = torch.ones((seq_len, seq_len))
         mask = 1 - torch.triu(mask, diagonal=1)
+        mask = mask.to(device=Q.device, dtype=Q.dtype)
         multiheadattn = scaled_dot_product_attention(Q, K, V, mask.bool())
 
         out = einops.rearrange(multiheadattn, "... h seq d_v -> ... seq (h d_v)")

tests/adapters.py
@@ -424,14 +424,15 @@ def run_transformer_lm(
         next-word distribution for each token.
     """
     lm = Transformer(
-        vocab_size,
-        context_length,
-        num_layers,
-        d_model,
-        num_heads,
-        d_ff,
-        rope_theta,
-        in_indices.device,
+        vocab_size=vocab_size,
+        context_length=context_length,
+        num_layers=num_layers,
+        d_model=d_model,
+        num_heads=num_heads,
+        d_ff=d_ff,
+        theta=rope_theta,
+        device=in_indices.device,
+        dtype=torch.float32,
     )
     lm.load_state_dict(weights)
 