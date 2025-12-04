96f643aad021f9d7fc70a698dad2bfa63ac3cdeb
Hadi Sheikhi <hadsheikhi@gmail.com>
2025-12-04 23:02:35 +0000

    able to get positions

--- src/data/utils.py
+++ src/data/utils.py
@@ -2,19 +2,26 @@ import torch
 import numpy as np
 
 
-def data_loading(x, batch_size, context_length, device="cpu"):
+def data_loading(x, batch_size, context_length, device="cpu", token_positions=False):
     starts = np.random.randint(
         low=0, high=x.shape[0] - context_length, size=(batch_size,)
     )
 
     x_sub = [x[s : s + context_length] for s in starts]
     y = [x[s + 1 : s + 1 + context_length] for s in starts]
+    positions = [np.arange(start=s, stop=s + context_length) for s in starts]
 
     x_sub = np.stack(x_sub)
     y = np.stack(y)
+    positions = np.stack(positions)
 
     x_sub = torch.tensor(x_sub, device=device)
     y = torch.tensor(y, device=device)
+
+    if token_positions:
+        positions = torch.tensor(positions, device=device)
+        return x_sub, y, positions
+
     return x_sub, y
 
 
